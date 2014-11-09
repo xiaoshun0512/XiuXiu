b0c470fe32dfa63d91bda5ac56d7cf8315093916
郭舜 <xiaoegg@kakushuntekiMacBook-Air.local>
2014-11-09 22:56:57 +0800

    SomeModify

--- app/src/main/java/com/example/xiaoegg/xiuxiu/MainActivity.java
+++ app/src/main/java/com/example/xiaoegg/xiuxiu/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.xiaoegg.xiuxiu;
 
 import android.os.Bundle;
 
+import com.example.xiaoegg.xiuxiu.Base.BaseActivity;
+
 
 /**
  * Created by xiaoegg on 14-11-4.
  */
-public class MainActivity extends android.support.v7.app.ActionBarActivity {
+public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
